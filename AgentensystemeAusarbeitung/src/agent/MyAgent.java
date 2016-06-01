3326dc9feef64e38193eeb534c1ebbd517c7bc8d
Benjamin Byl <BenjaminByl@web.de>
2016-06-01 21:22:03 +0200

    ...

--- AgentensystemeAusarbeitung/src/agent/MyAgent.java
+++ AgentensystemeAusarbeitung/src/agent/MyAgent.java
@@ -35,31 +35,18 @@ public class MyAgent extends Agent implements IAgent {
 	List<IBehaviour> behaviours;
 	private String worldName = "";
 	private IMap handler = new MapAsArray();
-	private String inToReplyTo="";
+	private String inToReplyTo = "";
+	private int state;
 	public static final Logger log = LoggerFactory.getLogger(MyAgent.class);
-	
 
 	@Override
 	protected void setup() {
-		/*Register the agent at the service antWorld2016, later maybe on some other services*/
-//		DFAgentDescription dfd = new DFAgentDescription();
-//		dfd.setName(this.getAID());
-//		// parse and create service description
-//		String[] services = { "antWorld2016" };
-//		for (int i = 0; i < services.length; ++i) {
-//			ServiceDescription sd = new ServiceDescription();
-//			sd.setType(services[i].trim());
-//			sd.setName(services[i].trim());
-//			dfd.addServices(sd);
-//		}
-//		try {
-//			DFService.register(this, dfd);
-//			log.info("reg");
-//		} catch (FIPAException e1) {
-//			e1.printStackTrace();
-//		}
-		
-		/*Searches for agents and services within antworld2016 => saves the found antWorld agent into worldName*/
+		state = 0;
+
+		/*
+		 * Searches for agents and services within antworld2016 => saves the
+		 * found antWorld agent into worldName
+		 */
 		addBehaviour(new OneShotBehaviour(this) {
 			@Override
 			public void action() {
@@ -71,10 +58,6 @@ public class MyAgent extends Agent implements IAgent {
 				DFAgentDescription[] search;
 				try {
 					search = DFService.search(myAgent, dfd);
-//					SearchConstraints searchConstraints = new SearchConstraints();
-//					searchConstraints.setMaxResults(1L);
-//					search = DFService.searchUntilFound(myAgent, new AID(AntWorldConsts.SEVICE_NAME,AID.ISLOCALNAME),dfd,searchConstraints, 90000000L);
-					log.info("buh");
 					for (int i = 0; i < search.length; i++) {
 						String localName = search[i].getName().getLocalName();
 						log.info(localName + ":");
@@ -85,73 +68,89 @@ public class MyAgent extends Agent implements IAgent {
 				} catch (FIPAException e) {
 					e.printStackTrace();
 				}
-				
-				/*Send logindata to the world, but still fails on it*/
-				addBehaviour(new OneShotBehaviour() {
-					@Override
-					public void action() {
-
-						Gson gson = new Gson();
-						sendMessage(gson.toJson(new LoginMessage(AntWorldConsts.ANT_ACTION_LOGIN)));
-					}
-				});
-				
 			}
-			
-			
+
 		});
-		
 
-
-		/*Is waiting for messages that arrive at the agent, he will just print the answer*/
+		/* Send logindata to the world, but still fails on it */
 		addBehaviour(new CyclicBehaviour() {
-			
+
+			@Override
+			public void action() {
+				Gson gson = new Gson();
+				switch (state) {
+				case (0):
+					sendMessage(gson.toJson(new LoginMessage(AntWorldConsts.ANT_ACTION_LOGIN)));
+					state = 9998;
+					break;
+				case (1):
+					sendMessage(gson.toJson(new LoginMessage(AntWorldConsts.ANT_ACTION_DOWN)));
+					state = 9999;
+					break;
+				case (2):
+					sendMessage(gson.toJson(new LoginMessage(AntWorldConsts.ANT_ACTION_LEFT)));
+					state = 9999;
+					break;
+				default:
+					block();
+					break;
+				}
+			}
+		});
+
+		/*
+		 * Is waiting for messages that arrive at the agent, he will just print
+		 * the answer
+		 */
+		addBehaviour(new CyclicBehaviour() {
 			@Override
 			public void action() {
 				MyAgent.log.info("Message Behaviour");
-				ACLMessage msg = myAgent.blockingReceive();
+				ACLMessage msg = myAgent.receive();
 				if (msg != null) {
 					msg.getContent();
 					msg.getSender();
 					inToReplyTo = msg.getReplyWith();
-					Gson gson = new Gson();
-					Message m = gson.fromJson(msg.getContent(), Message.class);
-					handler.addNewField(new Field(Integer.valueOf(m.currentFood), 0, 0, 0, 0, false, false), Direction.SOUTH);
-					MyAgent.log.info("ausgabe"+msg.getContent());
-				} else {
-					// block();
-				}
-				
-				/*Send movementdata to the world, but still fails on it*/
-				addBehaviour(new OneShotBehaviour() {
-					@Override
-					public void action() {
-						Gson gson = new Gson();
-						sendMessage(gson.toJson(new LoginMessage("ANT_ACTION_DOWN")));
+					log.info(inToReplyTo);
+					if (state == 9998) {
+						state = 1;
 					}
-				});
+					if (msg.getPerformative() == ACLMessage.INFORM) {
+						Gson gson = new Gson();
+						Message m = gson.fromJson(msg.getContent(), Message.class);
+						handler.addNewField(new Field(Integer.valueOf(m.currentFood), 0, 0, 0, 0, false, false),
+								Direction.SOUTH);
+						MyAgent.log.info("ausgabe" + msg.getContent());
+					}else if(msg.getPerformative() == ACLMessage.REFUSE){
+						state = 2;
+					}
+				} else {
+					block();
+				}
+
 			}
 		});
-		
 
 	}
 
 	/**
-	 * derigister the service 
+	 * derigister the service
 	 */
 	@Override
 	protected void takeDown() {
-//		try {
-//			DFAgentDescription dfd = new DFAgentDescription();
-//			dfd.setName(getAID());
-//			DFService.deregister(this, dfd);
-//			log.info("Client wird jetzt aus geschaltet");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		// try {
+		// DFAgentDescription dfd = new DFAgentDescription();
+		// dfd.setName(getAID());
+		// DFService.deregister(this, dfd);
+		// log.info("Client wird jetzt aus geschaltet");
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
 	}
+
 	/**
-	 * send a message to antWorld2016 
+	 * send a message to antWorld2016
+	 * 
 	 * @param Message
 	 */
 	private void sendMessage(String Message) {
@@ -161,7 +160,7 @@ public class MyAgent extends Agent implements IAgent {
 		msg.setInReplyTo(inToReplyTo);
 		msg.setContent(Message);
 		msg.setLanguage("JSON");
-		log.info("schicke: "+Message);
+		log.info("schicke: " + Message);
 		send(msg);
 	}
 
@@ -170,6 +169,5 @@ public class MyAgent extends Agent implements IAgent {
 		behaviours.add(new SearchBehaviour());
 		behaviours.add(new MessageBehaviour(this));
 	}
-
 
 }
